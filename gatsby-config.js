1d22eafca57ac1e0ac86b8434918b580b4d1e965
David Wesst <david@cocoboko.io>
2019-07-23 22:41:51 -0500

    added social media data

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,15 @@ module.exports = {
           }
         ]
       }
+    },
+    {
+      resolve: `gatsby-transformer-json`
+    },
+    { 
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/data/'
+      }
     }
   ]
 }
